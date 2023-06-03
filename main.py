afc8e8940d9ea68acd6430aaaae134ec982a0cf7
yoriichitsukiguni <samanyolu0233@gmail.com>
2023-06-03 18:56:52 +0300

    Mavi Yaka sınıfı için 3 nesne oluşturulması

--- main.py
+++ main.py
@@ -15,11 +15,17 @@ def main():
         issiz1 = Issiz("33333333333", "Mehmet", "Kara", 28, "Erkek", "Turk", {"mavi yaka": 2, "beyaz yaka": 4, "yonetici": 6})
         issiz2 = Issiz("44444444444", "Fatma", "Ak", 32, "Kadin", "Turk", {"mavi yaka": 3, "beyaz yaka": 5, "yonetici": 7})
         issiz3 = Issiz("55555555555", "Ali", "Beyaz", 29, "Erkek", "Turk", {"mavi yaka": 1, "beyaz yaka": 6, "yonetici": 8})
-        
+
         # Çalışan sınıfı için 3 nesne oluşturulması
         calisan1 = Calisan("66666666666", "Zeynep", "Kaya", 35, "Kadin", "Turk", "teknoloji", 36, 20000)
         calisan2 = Calisan("77777777777", "Mustafa", "Arslan", 40, "Erkek", "Turk", "muhaasebe", 48, 12000)
         calisan3 = Calisan("88888888888", "Ebru", "Yildirim", 27, "Kadin", "Turk", "insaat", 60, 25000)
+
+        # Mavi Yaka sınıfı için 3 nesne oluşturulması
+        mavi_yaka1 = MaviYaka("99999999999", "Deniz", "Demir", 33, "Erkek", "Turk", "teknoloji", 24, 15000, 0.2)
+        mavi_yaka2 = MaviYaka("10101010101", "Elif", "Yilmaz", 38, "Kadin", "Turk", "insaat", 42, 18000, 0.5)
+        mavi_yaka3 = MaviYaka("12121212121", "Ayhan", "Aktas", 31, "Erkek", "Turk", "teknoloji", 30, 14000, 0.3)
+
 
     except Exception as e:
         print("Hata olustu:", str(e))
