004da881912b0767bd2f175fd82ed935c2e0a7b6
yoriichitsukiguni <samanyolu0233@gmail.com>
2023-06-03 18:56:03 +0300

    İnsan sınıfı için 2 nesne oluşturulması

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ from Issiz import Issiz
 from Calisan import Calisan
 from MaviYaka import MaviYaka
 from BeyazYaka import BeyazYaka
+
+def main():
+    try:
+        # İnsan sınıfı için 2 nesne oluşturulması
+        insan1 = Insan("11111111111", "Ahmet", "Yilmaz", 30, "Erkek", "Turk")
+        insan2 = Insan("22222222222", "Ayse", "Demir", 25, "Kadin", "Turk")
+
+    except Exception as e:
+        print("Hata olustu:", str(e))
+        
+main()
